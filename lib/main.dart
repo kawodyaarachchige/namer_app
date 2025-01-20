b92a2c67e9b5f69c3c7f0acd4fe2b641e8dafed4
Tharushi Kawodya Arachchige <139765472+kawodyaarachchige@users.noreply.github.com>
2025-01-20 17:50:28 +0530

    code:added new page

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,8 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
@@ -62,7 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -186,3 +188,33 @@ class BigCard extends StatelessWidget {
 
 // ...
 }
+// ...
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
+    );
+  }
+}
+
